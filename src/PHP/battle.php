1e9e992e14af4630a285d9e940b7419e8f66de84
NysTabbers <Nystabbers2@gmail.com>
2025-12-10 11:20:29 +0100

    Made battle.php cleaner by using functions

--- src/PHP/battle.php
+++ src/PHP/battle.php
@@ -1,6 +1,6 @@
 <?php
 
-session_start(); 
+session_start();
 // Start een nieuwe sessie of hervat een bestaande sessie (nodig om data tussen pagina's te bewaren)
 
 require_once("spaceship.php");
@@ -10,7 +10,7 @@ require_once("stats.php");
 // Check of beide ruimteschepen zijn ingesteld
 if (!isset($spaceship) || !isset($spaceship2)) {
     $error = "Spaceships aren't set. Go to stats.php to set the stats";
-    die($error); 
+    die($error);
     // Stop script als stats ontbreken
 }
 
@@ -20,19 +20,71 @@ function shipAmmoRemaining($ship)
     return $ship->weapons->getAmmo();
 }
 
-function battleDone() {} 
-// Lege functie; lijkt bedoeld voor toekomstig gebruik (bijv. einde van gevecht)
+function battleDone($spaceship, $spaceship2)
+{
+    // Stop als een van de schepen geen HP meer heeft
+    if ($spaceship->getHP() <= 0 || $spaceship2->getHP() <= 0) {
+        return false;  // Battle stopt
+    }
+    // Stop als beide schepen geen munitie meer hebben
+    if (shipAmmoRemaining($spaceship) <= 0 && shipAmmoRemaining($spaceship2) <= 0) {
+        return false;  // Battle stopt
+    }
+    // Anders battle gaat door
+    return true;
+}
+
+// Function of de aanval raakt
+function hitChance($attacker, $weapons)
+{
+    $hitchance = $weapons->getHitChance();
+    $roll = rand(0, 10);
+    echo $attacker->getName() . " rolled a " . $roll . " to hit" . "<br>";
+    return $roll >= $hitchance;
+}
+
+function damageToDefender($defender, $damage)
+{
+    if ($defender->getShield() > 0) {
+        $shield = $defender->getShield();
+
+        // Als schade groter is dan shield → rest gaat naar hull
+        if ($damage >= $shield) {
+            $damageAfterShield = $damage - $shield;
+            $defender->shield = 0; //schild is volledig weg
+            echo "    -> " . $shield . " absorbed by shield (shield now 0)." . "<br>";
+            $damage = $damageAfterShield;
+        } else {
+            // Schild absorbeert alle schade
+            $defender->shield = $shield - $damage;
+            echo "    -> " . $damage . " absorbed by shield (shield now " . $defender->getShield() . ")." . "<br>";
+            $damage = 0; // Geen schade naar hull
+        }
+    }
+    if ($damage > 0) {
+        $hp = $defender->getHP();
+        $defender->hp = $hp - $damage;
+        echo $defender->getName() . " has " . $defender->hp . " hp left" . "<br>";
+    } else {
+        echo $defender->getName() . " took no hull damage." . "<br>";
+    }
+}
+
+function reverseRolls(&$attacker, &$defender)
+{
+    // Draait rollen om van attacker en defender
+    $temp = $attacker;
+    $attacker = $defender;
+    $defender = $temp;
+}
+
 
 // Stelt aanvaller en verdediger in voor de eerste ronde
 $attacker = $spaceship;
 $defender = $spaceship2;
 
 // Hoofdgevechtslus
-while (true) {
-    // Stop als een van de schepen geen HP meer heeft
-    if ($spaceship->getHP() <= 0 || $spaceship2->getHP() <= 0) break;
-    // Stop als beide schepen geen munitie meer hebben
-    if (shipAmmoRemaining($spaceship) <= 0 && shipAmmoRemaining($spaceship2) <= 0) break;
+while (battleDone($spaceship, $spaceship2)) {
 
     $weapons = $attacker->weapons;    // Verkrijg wapenobject van het aanvallende schip
     $weaponName = $weapons->getName(); // Naam van het wapen (nog niet gebruikt)
@@ -45,42 +97,12 @@ while (true) {
         // Gebruik één munitiepunt
         $attacker->weapons->ammo = $ammo - 1;
 
-        // Bepaal of de aanval raakt
-        $hitchance = $weapons->getHitChance(); // Minimale worp die nodig is om te raken
-        $roll = rand(0, 10);                   // Willekeurige worp tussen 0 en 10
-        echo $attacker->getName() . " rolled a " . $roll . " to hit" . "<br>";
-
-        if ($roll >= $hitchance) {
+        if (hitChance($attacker, $weapons)) {
             // Aanval raakt
             $damage = $weapons->getAttackDamage(); // Basis schade
             echo "  -> HIT! " . $attacker->getName() . " deals " . $damage . " damage." . "<br>";
 
-            // Eerst shields aftrekken
-            if ($defender->getShield() > 0) {
-                $shield = $defender->getShield();
-
-                // Als schade groter is dan shield → rest gaat naar hull
-                if ($damage >= $shield) {
-                    $damageAfterShield = $damage - $shield;
-                    $defender->shield = 0; // Schild is volledig weg
-                    echo "    -> " . $shield . " absorbed by shield (shield now 0)." . "<br>";
-                    $damage = $damageAfterShield;
-                } else {
-                    // Schild absorbeert alle schade
-                    $defender->shield = $shield - $damage;
-                    echo "    -> " . $damage . " absorbed by shield (shield now " . $defender->getShield() . ")." . "<br>";
-                    $damage = 0; // Geen schade naar hull
-                }
-            }
-
-            // Overgebleven schade naar hull HP
-            if ($damage > 0) {
-                $hp = $defender->getHP();
-                $defender->hp = $hp - $damage;
-                echo $defender->getName() . " has " . $defender->hp . " hp left" . "<br>";
-            } else {
-                echo $defender->getName() . " took no hull damage." . "<br>";
-            }
+            damageToDefender($defender, $damage);
         } else {
             // Aanval mist
             echo "  -> MISS! " . $attacker->getName() . " did not hit." . "<br>";
@@ -88,7 +110,5 @@ while (true) {
     }
 
     // Rollen omwisselen: aanvaller wordt verdediger voor volgende beurt
-    $temp = $attacker;
-    $attacker = $defender;
-    $defender = $temp;
+    reverseRolls($attacker, $defender);
 }
